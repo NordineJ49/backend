ef996c62b1cff9a30ba423127e05dacb9e878550
NordineJ49 <nordine.j49@gmail.com>
2023-01-23 17:43:01 +0100

    crud

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+// importe le module Express
 const express = require('express');
 
-const app = express();
 
-mongoose.connect('mongodb+srv://NordineJ:<blabla49>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+// importe le module mongoose
+const mongoose = require('mongoose');
+
+const Thing = require('./models/thing');
+
+
+// process.env + nom de la variable
+mongoose.connect('mongodb+srv://NordineJ:blabla49@cluster0.xwwpyzo.mongodb.net/?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -10,10 +17,12 @@ mongoose.connect('mongodb+srv://NordineJ:<blabla49>@cluster0-pme76.mongodb.net/t
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const mongoose = require('mongoose');
+// const app qui utilise express() pour gerer les routes et les middlewares (app.use etc ...)
+const app = express();
 
-app.use(express.json())
+// methode use de l'appli express pour ajouter un middleware express.json(), qui permet de traiter les données envoyées au serveur dans un format JSON
 
+// methode use de l'appli express pour ajouter un middlkeware qui permet de definir les headers pour gérer les problèmes d'accès lié a la sécurité
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -21,33 +30,43 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'objet créé'
-    })
-})
+app.use(express.json())
 
-app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    res.status(200).json(stuff);
+// methode post pour ajouter une route pour les requetes http de type post
+app.post('/api/stuff', (req, res, next) => {
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
+    });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
 });
 
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error }));
+});
+
+// methode get de l'appli express pour ajouter une route pour les requetes http de type get
+app.get('/api/stuff', (req, res, next) => {
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }));
+});
+
+// objet module de node qui exporte la variable app (express())
 module.exports = app;
--- server.js
+++ server.js
@@ -1,20 +1,29 @@
+// import des modules http et app
 const http = require('http');
 const app = require('./app');
 
+// Fonction normalizePort qui prend une valeur de numéro de port sur lequel le serveur doit écouter
 const normalizePort = val => {
     const port = parseInt(val, 10);
-
+    // si 'port' n'est pas un nombre valide -------- (isNaN = is Not a Number)
     if (isNaN(port)) {
+        // si c'est le cas retourne la valeur d'entrée non modifiée
         return val;
     }
+    // verifie si la valeur convertie est supérieur ou égale à zero et si oui retourne 'port'
     if (port >= 0) {
         return port;
     }
+    // sinon retourne false
     return false;
 };
+
+// variable qui défini sur quel port le serveur doit écouter
 const port = normalizePort(process.env.PORT || '3000');
+// methode set de l'application Express pour définir la propriété 'port' sur la valeur de la variable 'port'
 app.set('port', port);
 
+// fonction errorHandler qui gère les erreurs
 const errorHandler = error => {
     if (error.syscall !== 'listen') {
         throw error;
